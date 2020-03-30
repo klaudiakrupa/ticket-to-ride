b276dd52b118b63b2a211d25878b8164a48d05ef
Klaudia Krupa <eklaudiakrupa@gmail.com>
2020-03-30 10:35:50 +0200

    Enter information about cities and connection

---
+++ src/components/Map/geoCordinates.js
@@ -0,0 +1,125 @@
+export const Lines = [
+  { top: "12rem", rotate: "-10deg", left: "59rem", color: "green", lenght: 5 },
+  { top: "18rem", rotate: "127deg", left: "59.8rem", color: "red", lenght: 6 },
+  { top: "19.5rem", rotate: "70deg", left: "55rem", color: "black", lenght: 4 },
+  {
+    top: "16.4rem",
+    rotate: "126deg",
+    left: "53.3rem",
+    color: "blue",
+    lenght: 2
+  },
+  {
+    top: "22.3rem",
+    rotate: "35deg",
+    left: "54.2rem",
+    color: "green",
+    lenght: 3
+  },
+  { top: "23.5rem", rotate: "122deg", left: "49rem", color: "blue", lenght: 2 },
+  {
+    top: "26.5rem",
+    rotate: "-8deg",
+    left: "50.5rem",
+    color: "black",
+    lenght: 4
+  },
+  {
+    top: "28.6rem",
+    rotate: "118deg",
+    left: "58.1rem",
+    color: "blue",
+    lenght: 2
+  },
+  { top: "29.8rem", rotate: "25deg", left: "50rem", color: "red", lenght: 3 },
+  { top: "37rem", rotate: "90deg", left: "55rem", color: "green", lenght: 3 },
+  { top: "16rem", rotate: "-18deg", left: "43.6rem", color: "red", lenght: 5 },
+  { top: "19rem", rotate: "5deg", left: "44rem", color: "black", lenght: 3 },
+  { top: "21rem", rotate: "84deg", left: "42rem", color: "blue", lenght: 1 },
+  { top: "25.5rem", rotate: "36deg", left: "44rem", color: "red", lenght: 2 },
+  {
+    top: "44.5rem",
+    rotate: "-39deg",
+    left: "53.3rem",
+    color: "red",
+    lenght: 2
+  },
+  {
+    top: "41.8rem",
+    rotate: "60deg",
+    left: "44.2rem",
+    color: "green",
+    lenght: 4
+  },
+  { top: "34rem", rotate: "-19deg", left: "47rem", color: "blue", lenght: 4 },
+  { top: "32.5rem", rotate: "68deg", left: "42.5rem", color: "red", lenght: 2 },
+  {
+    top: "28.5rem",
+    rotate: "-12deg",
+    left: "45.5rem",
+    color: "black",
+    lenght: 1
+  },
+  { top: "29rem", rotate: "-4deg", left: "35rem", color: "blue", lenght: 3 },
+  { top: "35rem", rotate: "65deg", left: "31rem", color: "green", lenght: 4 },
+  { top: "33rem", rotate: "160deg", left: "13rem", color: "blue", lenght: 7 },
+  { top: "36.5rem", rotate: "3deg", left: "3.5rem", color: "red", lenght: 3 },
+  {
+    top: "26rem",
+    rotate: "148deg",
+    left: "34.5rem",
+    color: "black",
+    lenght: 3
+  },
+  {
+    top: "30rem",
+    rotate: "136deg",
+    left: "11.3rem",
+    color: "black",
+    lenght: 6
+  },
+  { top: "23.5rem", rotate: "-2deg", left: "28rem", color: "green", lenght: 5 },
+  { top: "19rem", rotate: "71deg", left: "21rem", color: "blue", lenght: 3 },
+  { top: "11.5rem", rotate: "45deg", left: "18rem", color: "green", lenght: 2 },
+  { top: "15.5rem", rotate: "17deg", left: "25rem", color: "red", lenght: 2 },
+  { top: "20rem", rotate: "28deg", left: "31.5rem", color: "black", lenght: 4 },
+  { top: "13rem", rotate: "135deg", left: "31rem", color: "green", lenght: 3 },
+  { top: "14rem", rotate: "66deg", left: "37rem", color: "black", lenght: 3 },
+  { top: "5.5rem", rotate: "118deg", left: "38rem", color: "red", lenght: 2 },
+  {
+    top: "1.5rem",
+    rotate: "-5deg",
+    left: "43.5rem",
+    color: "green",
+    lenght: 4
+  },
+  { top: "2.5rem", rotate: "165deg", left: "52rem", color: "black", lenght: 1 },
+  { top: "6.5rem", rotate: "154deg", left: "39.5rem", color: "blue", lenght: 4 }
+];
+
+export const Dots = [
+  { name: "Moskwa", top: "9.5rem", left: "73.5rem" },
+  { name: "Helsinki", top: "0.5rem", left: "55rem" },
+  { name: "Oslo", top: "1rem", left: "43rem" },
+  { name: "Sztokholm", top: "2rem", left: "49.5rem" },
+  { name: "Kopenhaga", top: "8.5rem", left: "37.5rem" },
+  { name: "Berlin", top: "17.5rem", left: "43rem" },
+  { name: "Wilno", top: "12rem", left: "58rem" },
+  { name: "Warszawa", top: "18.5rem", left: "52.5rem" },
+  { name: "Praga", top: "22.5rem", left: "43.5rem" },
+  { name: "Bratysława", top: "26.3rem", left: "48rem" },
+  { name: "Kijów", top: "24.3rem", left: "62.5rem" },
+  { name: "Bukareszt", top: "30.7rem", left: "58rem" },
+  { name: "Sarajewo", top: "34.8rem", left: "45.7rem" },
+  { name: "Sofia", top: "40.8rem", left: "58.5rem" },
+  { name: "Ateny", top: "45.5rem", left: "53rem" },
+  { name: "Wiedeń", top: "27.7rem", left: "43.8rem" },
+  { name: "Rzym", top: "39.3rem", left: "39rem" },
+  { name: "Berno", top: "27.7rem", left: "33.5rem" },
+  { name: "Paryż", top: "22.9rem", left: "27rem" },
+  { name: "Londyn", top: "12.8rem", left: "23.5rem" },
+  { name: "Dublin", top: "7.5rem", left: "18rem" },
+  { name: "Lizbona", top: "34.8rem", left: "1.9rem" },
+  { name: "Madryt", top: "35rem", left: "12rem" },
+  { name: "Amsterdam", top: "15.5rem", left: "30rem" }
+];
